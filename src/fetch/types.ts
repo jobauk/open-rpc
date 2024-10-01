fd6c1ae8316aac92b77d5d3ba196101feb625c7c
Snuifstuif <job.aukes@gmail.com>
2024-10-01 12:25:21 +0200

    fix: allow for root level functions

--- src/fetch/types.ts
+++ src/fetch/types.ts
@@ -92,7 +92,7 @@ export type ExtractFunctions<T> = UnionToTuple<T> extends [
   (...args: infer P) => infer R,
   ...infer Rest,
 ]
-  ? ((...args: P) => R) | ExtractFunctions<Rest>
+  ? ((...args: P) => R) & ExtractFunctions<Rest>
   : // biome-ignore lint/suspicious/noExplicitAny: <explanation>
     T extends [...any, (...args: infer P) => infer R]
     ? (...arg: P) => R
